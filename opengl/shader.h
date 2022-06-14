2cd0094006a92ff133423005cd2781f901051218
Albert <15221024927@163.com>
2022-06-15 01:35:34 +0800

    opengl
    Signed-off-by: Albert <15221024927@163.com>

---
+++ opengl/shader.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+class Shader
+{
+public:
+	unsigned int ID;
+	Shader(const char* vertexPath, const char* fragmentPath);
+	void use();
+	void setBool(const std::string& name, bool value);
+	void setInt(const std::string& name, int value);
+	void setFloat(const std::string& name, float value);
+
+private:
+	void checkCompileErrors(unsigned int shader, std::string type);
+
+};
+
