664f8a70900da6fc2f95b619716a80e3e901ab8a
Albert <15221024927@163.com>
2022-06-15 17:22:16 +0800

    camera
    Signed-off-by: Albert <15221024927@163.com>

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -210,6 +210,14 @@ int main()
         // render container
         glBindVertexArray(VAO);
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+
+        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+        ourShader.setMat4("view", view);
+
         for (unsigned int i = 0; i < 10; i++)
         {
             glm::mat4 model = glm::mat4(1.0f);
@@ -220,6 +228,7 @@ int main()
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
+
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
