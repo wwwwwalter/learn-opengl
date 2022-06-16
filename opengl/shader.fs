f5b90752c898bf311c31cfe1fbcc83f176c184a6
Albert <15221024927@163.com>
2022-06-16 10:51:28 +0800

    light
    Signed-off-by: Albert <15221024927@163.com>

--- opengl/shader.fs
+++ opengl/shader.fs
@@ -1,10 +1,9 @@
 #version 330 core
-out vec4 FragColor;  
+out vec4 FragColor;
 
 
-in vec2 TexCoord;
-
-uniform sampler2D texture1;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
 
 
 
@@ -13,5 +12,6 @@ void main()
     //FragColor = vec4(ourColor, 1.0);
     //FragColor = texture(ourTexture,TexCoord);
     //FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5);
-    FragColor = texture(texture1,TexCoord);
+    //FragColor = texture(texture1,TexCoord);
+    FragColor = vec4(lightColor * objectColor, 1.0);
 }
