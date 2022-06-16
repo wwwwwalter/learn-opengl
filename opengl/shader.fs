8954e8ff4c23f55246235eeee4c42a7198d9373a
Albert <15221024927@163.com>
2022-06-16 11:59:46 +0800

    light
    Signed-off-by: Albert <15221024927@163.com>

--- opengl/shader.fs
+++ opengl/shader.fs
@@ -1,7 +1,10 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 Normal;
+in vec3 FragPos;
 
+uniform vec3 lightPos;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 
@@ -9,9 +12,16 @@ uniform vec3 lightColor;
 
 void main()
 {
-    //FragColor = vec4(ourColor, 1.0);
-    //FragColor = texture(ourTexture,TexCoord);
-    //FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5);
-    //FragColor = texture(texture1,TexCoord);
-    FragColor = vec4(lightColor * objectColor, 1.0);
+	//ambient
+	float ambientStrength = 0.1;
+	vec3 ambient = ambientStrength * lightColor;
+
+	//diffuse
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightPos - FragPos);
+	float diff = max(dot(norm,lightDir),0.0);
+	vec3 diffuse = diff * lightColor;
+
+	vec3 result = (ambient + diffuse) * objectColor;
+	FragColor = vec4(result,1.0);
 }
--- opengl/shader.vs
+++ opengl/shader.vs
@@ -1,5 +1,9 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;   // 位置变量的属性 位置值为 0 
+layout (location = 0) in vec3 aNormal;
+
+out vec3 FragPos;
+out vec3 Normal;
 
 
 uniform mat4 model;
@@ -8,5 +12,7 @@ uniform mat4 projection;
 
 void main()
 {
+    FragPos = vec3(model * vec4(aPos,1.0));
+    Normal = aNormal;
     gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
